1a94911f9fc6d6db3b9df2020162fc2e447c656d
eduardovighi8@gmail.com <seu-email>
2025-11-06 16:27:58 -0300

    feat(db): ConfiguracaoSistema

---
+++ api_agendamento/prisma/migrations/20251106_configuracao_multa_padrao/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "public"."ConfiguracaoSistema" (
+    "id" INTEGER NOT NULL,
+    "valorMultaPadrao" DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT "ConfiguracaoSistema_pkey" PRIMARY KEY ("id")
+);
+
