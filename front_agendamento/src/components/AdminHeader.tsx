3da5dc90f84967963a5ce7455321e0aab34bb9e4
eduardovighi8@gmail.com <seu-email>
2025-12-08 19:54:45 -0300

    clareando mais a borda

--- front_agendamento/src/components/AdminHeader.tsx
+++ front_agendamento/src/components/AdminHeader.tsx
@@ -63,7 +63,7 @@ export default function AdminHeader() {
       <div>
         <div className="max-w-6xl mx-auto bg-white">
           {/* HEADER SUPERIOR */}
-          <header className="text-gray-800 px-4 py-3 flex items-center justify-between border-b-2 border-gray-400">
+          <header className="text-gray-800 px-4 py-3 flex items-center justify-between border-b-2 border-gray-350">
             <Link
               href="/adminMaster"
               aria-label="Ir para o painel Admin Master"
