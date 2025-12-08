9f84fd4e3259053231e731db56085563825913ef
eduardovighi8@gmail.com <seu-email>
2025-12-08 17:49:43 -0300

    mais uma alteração nos icones(ultima)

--- front_agendamento/src/app/adminMaster/page.tsx
+++ front_agendamento/src/app/adminMaster/page.tsx
@@ -1094,7 +1094,7 @@ export default function AdminHome() {
 
                             {!q.disponivel && !q.bloqueada && isComum && (
                               <Image
-                                src="/iconescards/icone_reservado.png"
+                                src="/iconescards/icone-reservado.png"
                                 alt="Reserva avulsa"
                                 width={32}
                                 height={32}
