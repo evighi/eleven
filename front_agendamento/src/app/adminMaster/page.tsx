63a3b921857ffce00612501570b1d8237e40e1a9
eduardovighi <eduardovighi8@gmail.com>
2025-12-11 19:04:22 -0300

    esticando texto

--- front_agendamento/src/app/adminMaster/page.tsx
+++ front_agendamento/src/app/adminMaster/page.tsx
@@ -2319,7 +2319,7 @@ export default function AdminHome() {
       {/* MODAL: Confirmar agendamento (quadra livre) */}
       {mostrarConfirmaAgendar && preReserva && (
         <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-[70]">
-          <div className="bg-white rounded-md shadow-2xl w-full max-w-md mx-4 p-10 relative">
+          <div className="bg-white rounded-md shadow-2xl w-full max-w-lg mx-4 p-10 relative">
             {/* X para fechar */}
             <button
               onClick={() => setMostrarConfirmaAgendar(false)}
@@ -2372,7 +2372,7 @@ export default function AdminHome() {
       {/* MODAL: Confirmar agendamento (churrasqueira livre) */}
       {mostrarConfirmaChurras && preReservaChurras && (
         <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-[70]">
-          <div className="bg-white rounded-md shadow-2xl w-full max-w-md mx-4 p-10 relative">
+          <div className="bg-white rounded-md shadow-2xl w-full max-w-lg mx-4 p-10 relative">
             {/* X para fechar */}
             <button
               onClick={() => setMostrarConfirmaChurras(false)}
