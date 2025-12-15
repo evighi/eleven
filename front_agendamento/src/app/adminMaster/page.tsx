94c1ddeed713aebbac6dba21b693d0bda9d488d2
eduardovighi <eduardovighi8@gmail.com>
2025-12-15 19:58:30 -0300

    aumentando ainda mais

--- front_agendamento/src/app/adminMaster/page.tsx
+++ front_agendamento/src/app/adminMaster/page.tsx
@@ -2652,7 +2652,7 @@ export default function AdminHome() {
             </div>
 
             {/* RODAPÉ – BOTÕES CANCELAR / INSERIR */}
-            <div className="mt-8 flex justify-center gap-[90px]">
+            <div className="mt-8 flex justify-center gap-[120px]">
               <button
                 onClick={() => !addingPlayers && setAbrirModalJogadores(false)}
                 disabled={addingPlayers}
