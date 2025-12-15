4f01449d678644bb3820b44a5b03d65f338aa498
eduardovighi <eduardovighi8@gmail.com>
2025-12-15 19:53:16 -0300

    aumentando espaçamento entre os botoes de cancelar e inserir

--- front_agendamento/src/app/adminMaster/page.tsx
+++ front_agendamento/src/app/adminMaster/page.tsx
@@ -2652,7 +2652,7 @@ export default function AdminHome() {
             </div>
 
             {/* RODAPÉ – BOTÕES CANCELAR / INSERIR */}
-            <div className="mt-8 flex justify-center gap-10">
+            <div className="mt-8 flex justify-center gap-[60px]">
               <button
                 onClick={() => !addingPlayers && setAbrirModalJogadores(false)}
                 disabled={addingPlayers}
